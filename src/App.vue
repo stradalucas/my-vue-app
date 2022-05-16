a77d8ccee2e232c5c4a144967522b82957b8497f
StradaLucas <fxloopsl@hotmail.com>
2022-05-16 15:55:14 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>{{ mensaje }}</h1>    
+    <listado-articulos></listado-articulos>
+  </div>
+</template>
+
+<script>
+import ListadoArticulos from './components/ListadoArticulos.vue'
+export default {
+  components: {
+    'listado-articulos': ListadoArticulos
+  },
+  name: 'app',
+  data () {
+    return {
+      mensaje: 'Listado de productos mediante petición Ajax'
+    }
+  }
+}
+</script>
+
+<style>
+  h1 {
+    font-size: 20px;
+    text-decoration: underline;
+  }
+</style>
---
+++ src/components/ListadoArticulos.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <table border="1">
+        <thead>
+          <tr>
+            <th>Código</th>
+            <th>Descripción</th>
+            <th>Precio</th>
+          </tr>
+        </thead>  
+        <tr v-for="articulo in articulos">
+          <td>{{articulo.codigo}}</td>
+          <td>{{articulo.descripcion}}</td>
+          <td>{{articulo.precio}}</td>
+       </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  created() {
+       axios.get('http://scratchya.com.ar/vue/datos.php')
+            .then((respuesta) => {
+              this.articulos = respuesta.data;
+            })
+  },
+  data() {
+    return {
+      articulos: []
+    }
+  }  
+}
+</script>
+
+<style>
+table {
+  border-collapse: collapse;
+  padding: 0.75rem;
+  vertical-align: top;
+  border-top: 1px solid #dee2e6;  
+}
+
+tr:nth-of-type(odd) {
+  background-color: rgba(0, 0, 0, 0.05);
+}
+
+tr:hover {
+  background-color: rgba(0, 0, 0, 0.075);
+}
+</style>
